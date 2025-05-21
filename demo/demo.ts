ff1bb659eb803a23ed5f038e4f90d884c2edfd20
prajeesh <prajeeshag@gmail.com>
2025-05-21 20:24:44 +0300

    add zarr files and able read

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,8 +1,10 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-
-// Import your plugin from relative path
+import * as zarr from "zarrita";
 
 // Create map and test plugin
 const map = L.map("map").setView([0, 0], 2);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+const baseUrl = `${window.location.origin}/wind.zarr/`;
+var store = await zarr.open.v3(new zarr.FetchStore(baseUrl + "U10/"));
