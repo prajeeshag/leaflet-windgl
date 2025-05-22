5afecbc0ae8b95b7acaf8a4683551e7dd330fdc7
prajeesh <prajeeshag@gmail.com>
2025-05-22 18:55:31 +0300

    updated

--- src/wind/shaders/update.frag.glsl
+++ src/wind/shaders/update.frag.glsl
@@ -33,8 +33,8 @@ void main() {
 
     // take EPSG:4236 distortion into account for calculating where the particle moved
     //float distortion = cos(radians(pos.y * 180.0 - 90.0));
-    float distortion = 1.0;
-    vec2 offset = vec2(velocity.x / distortion, -velocity.y) * 0.0001 * u_speed_factor;
+    // float distortion = 1.0;
+    vec2 offset = vec2(velocity.x, -velocity.y) * 0.0001 * u_speed_factor;
 
     // update particle position, wrapping around the date line
     pos = fract(1.0 + pos + offset);
--- src/wind/shaders/updateProp.frag.glsl
+++ src/wind/shaders/updateProp.frag.glsl
@@ -23,8 +23,9 @@ void main() {
     // float speed_age = step(length(velocity), 0.5);
     float speed_age = 1. / max(length(velocity), 0.000001);
     vec4 color1 = texture2D(u_particle_props, v_tex_pos);
-    float age = (color1.r + color1.g / 255.0) + u_drop_rate + (0.01 * speed_age);
-    age = age * step(0.0, 1.0 - age);
+    float age = (color1.r + color1.g / 255.0);
+    age = age + u_drop_rate + (0.01 * speed_age);
+    age = age * step(0.0, 1.0 - age); // kill particles that are older than 1.0
 
     // encode the new particle position back into RGBA
     gl_FragColor = vec4(vec2(floor(age * 255.0) / 255.0, fract(age * 255.0)), 0, 0);
