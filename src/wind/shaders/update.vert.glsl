946cf76054eb57a472fe44220114c6abbc9d66ec
prajeesh <prajeeshag@gmail.com>
2025-05-25 05:11:05 +0300

    updated

---
+++ src/wind/shaders/update.vert.glsl
@@ -0,0 +1,14 @@
+#version 100
+precision mediump float;
+
+attribute vec2 a_index;
+
+uniform vec2 u_particles_res;
+
+varying vec2 v_tex_pos;
+
+void main() {
+    v_tex_pos = a_index;
+    gl_PointSize = 1.0;
+    gl_Position = vec4(v_tex_pos, 0, 1);
+}
